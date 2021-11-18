694cb383c3fa29108f7ff7e117757c67d82065cb
tzivadinovic <zivadinovictomislav@gmail.com>
2021-11-18 19:37:44 +0100

    fixed lock script for two monitor setup

--- src/lock.sh
+++ src/lock.sh
@@ -7,8 +7,8 @@ revert() {
 TMPBG="/tmp/screen.png"
 scrot "$TMPBG"
 convert "$TMPBG" -scale 10% -scale 1000% "$TMPBG"
-convert -gravity center -font "Fira-Code-Bold-Nerd-Font-Complete-Mono" \
-	-pointsize 256 -draw "text 0,0 ''" -channel RGBA -fill '#EBCB8B' \
+convert -font "Fira-Code-Bold-Nerd-Font-Complete-Mono" \
+	-pointsize 256 -draw "text 875,600 ''" -channel RGBA -fill '#EBCB8B' \
 	"$TMPBG" "$TMPBG"
 trap revert HUP INT TERM
 playerctl pause
