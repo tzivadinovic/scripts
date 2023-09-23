849c3acd494ea373e7ba00b9f410f6f2115ef0d6
tzivadinovic <zivadinovictomislav@gmail.com>
2023-09-23 16:54:13 +0200

    changes

--- src/statusbar/weather.sh
+++ src/statusbar/weather.sh
@@ -5,7 +5,7 @@ SWITCH="$HOME/.cache/statusbar_$(basename $0)"
 
 show_weather() {
 	#i3-msg "workspace weather"
-	curl -s http://wttr.in/"$WEATHER" > /tmp/weather 2>&1
+	curl -s http://wttr.in/"Nis" > /tmp/weather 2>&1
 	i3-msg "exec --no-startup-id setsid -f st -c weather_floating -e less -Srf /tmp/weather" 2>/dev/null 1>/dev/null
 }
 
